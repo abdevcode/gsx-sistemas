3350ee32c76868673f1d89ebb80c7e76548cacbf
MaxLeat <57955198+MaxLeat@users.noreply.github.com>
2020-02-26 17:06:50 +0100

    Fa la compressio i la busqueda als dos directoris que demana

--- lab2/test_service.sh
+++ lab2/test_service.sh
@@ -15,63 +15,67 @@ minut_i=${temps_i:3:2}
 
 if [ ! -d "/back" ]; then
 
-	(mkdir "/back")
+	mkdir "/back"
 fi
-(mkdir "/back/$DATE")
+mkdir "/back/$DATE"
+
+#Busquem als directoris
+
+directori_busca="/usr"
+contador=0
 echo "S'esta buscant els fitxers que han sigut modificats..."
 
-#For al directori /usr
-for ruta in $(find /home/milax/GSX/lab2 \! -name ".*");do
-	
-	#echo "\"$ruta\"" 
-	if [ -f $ruta ]; then
-		data=$((stat -c  %y $ruta) | awk '{print $1}') 
-		any=${data:0:4}
-		mes=${data:5:2}
-		dia=${data:8:2}
-		temps=$((stat -c  %y $ruta) | awk '{print $2}') 
-		hora=${temps:0:2}
-		minut=${temps:3:2}
-		#mirem si el fitxer del path ha sigut modificat
-		if [ $any -gt $any_i ]; then
-			echo $ruta >> paths.txt
-		elif [ $any -eq $any_i ]; then
-			if [ $mes -gt $mes_i ]; then
+while [ $contador -lt 2 ]; do
+	for ruta in $(find $directori_busca \! -name ".*");do
+		#echo "\"$ruta\"" 
+		if [ -f $ruta ]; then
+			data=$((stat -c  %y $ruta) | awk '{print $1}') 
+			any=${data:0:4}
+			mes=${data:5:2}
+			dia=${data:8:2}
+			temps=$((stat -c  %y $ruta) | awk '{print $2}') 
+			hora=${temps:0:2}
+			minut=${temps:3:2}
+			#mirem si el fitxer del path ha sigut modificat
+			if [ $any -gt $any_i ]; then
 				echo $ruta >> paths.txt
-			elif [ $mes -eq $mes_i ]; then
-				if [ $dia -gt $dia_i ]; then
+			elif [ $any -eq $any_i ]; then
+				if [ $mes -gt $mes_i ]; then
 					echo $ruta >> paths.txt
-				elif [ $dia -eq $dia_i ]; then
-					if [ $hora -gt $hora_i ]; then
+				elif [ $mes -eq $mes_i ]; then
+					if [ $dia -gt $dia_i ]; then
 						echo $ruta >> paths.txt
-					elif [ $hora -eq $hora_i ]; then
-						if [ $minut -gt $minut_i ]; then
+					elif [ $dia -eq $dia_i ]; then
+						if [ $hora -gt $hora_i ]; then
 							echo $ruta >> paths.txt
-							nom=$(basename $ruta)
-							echo "$nom"
-							(cp $ruta /back/$DATE/$nom)
-						fi
-					fi 
+						elif [ $hora -eq $hora_i ]; then
+							if [ $minut -gt $minut_i ]; then
+								echo $ruta >> paths.txt
+								nom=$(basename $ruta)
+								echo "$nom"
+								cp $ruta /back/$DATE/$nom
+							fi
+						fi 
+					fi
 				fi
 			fi
 		fi
-	fi
+	done
+	directori_busca="/home"
+	let contador=$contador+1
 done
 
 #Executem el script per guardar els permisos i tal i ho redirigim a un fitxer de text
-. gpgp.sh paths >> resultat.txt
+. gpgp.sh paths.txt >> resultat.txt
+#Copiem la resta dels fitxers a la carpeta
+cp paths.txt /back/$DATE/paths.txt
+cp resultat.txt /back/$DATE/resultat.txt
+#Ens movem a la zona on esta el backup
+cd /back
+#comprimim en tgz
+sudo tar -czvf $DATE.tar.gz $DATE
+#Esborrem la carpeta anterior per deixar nomes el fitxer comprimit
+sudo rm -r $DATE
 
-#fer un for ==> 
-#fer un for ==> find /home
 
 
-
-
-echo "Example service started at ${DATE}" | systemd-cat -p info
-<< ////
-while :
-do
-echo "Looping...";
-sleep 30;
-done
-////
