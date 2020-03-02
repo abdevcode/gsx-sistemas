319c8885509449cfcf1e282588ba58cb8e3dc9c6
abdevcode <abdevcode@gmail.com>
2020-03-03 00:08:55 +0100

    abdevcode: Modificado el fichero test_service.sh

--- lab2/test_service.sh
+++ lab2/test_service.sh
@@ -1,23 +1,68 @@
 #! /bin/bash
 
+# Crear la carpeta de las copias de seguridad
+mkdir -p "/back" # -p : Crea els directoris pare necessaris, sense mostrar errors si aquests ja existeixen.
+date_act=$(date '+%y%m%d%H%M')
+dir_back="/back/$date_act"
+mkdir $dir_back
+
+
+
+
+
+# Para saber la fecha actual
+up_time=$(uptime -s)
+udate_start=$(date -d "$up_time" +%s) # Hora UNIX de la fecha 
+
+# Directorios que queremos guardar separados por espacio en blanco
+dir_save="/home"
+
+for directori_busca in $dir_save; do
+	for ruta in $(find $directori_busca -type d -name ".*" -prune -o -type f \! -name ".*" -print); do
+		if [ -f $ruta ]; then
+			date_file=$(date -r "$ruta" "+%Y-%m-%d %H:%M:%S")
+			udate_file=$(date -d "$date_file" +%s) # Hora UNIX de la fecha 
+			
+			if [ $udate_file -gt $udate_start ]; then
+				echo $ruta
+			fi 
+		fi
+	done
+done
+
+
+#Executem el script per guardar els permisos i tal i ho redirigim a un fitxer de text
+. gpgp.sh paths.txt >> resultat.txt
+#Copiem la resta dels fitxers a la carpeta
+cp paths.txt /back/$DATE/paths.txt
+cp resultat.txt /back/$DATE/resultat.txt
+#Ens movem a la zona on esta el backup
+cd /back
+#comprimim en tgz
+sudo tar -czvf $DATE.tar.gz $DATE
+#Esborrem la carpeta anterior per deixar nomes el fitxer comprimit
+sudo rm -r $DATE
+
+
+
+exit
+exit 
+exit
+
 #DATE=`date '+%Y-%m-%d %H:%M:%S'`
-DATE=`date '+%y%m%d%H%M'`
-data_inici=$((stat -c  %y /etc/resolv.conf) | awk '{print $1}')
-any_i=${data_inici:0:4}
-mes_i=${data_inici:5:2}
-dia_i=${data_inici:8:2}
-temps_i=$((stat -c  %y /etc/resolv.conf) | awk '{print $2}') 
-hora_i=${temps_i:0:2}
-minut_i=${temps_i:3:2}
+#DATE=`date '+%y%m%d%H%M'`
+#data_inici=$((stat -c  %y /etc/resolv.conf) | awk '{print $1}')
+#any_i=${data_inici:0:4}
+#mes_i=${data_inici:5:2}
+#dia_i=${data_inici:8:2}
+#temps_i=$((stat -c  %y /etc/resolv.conf) | awk '{print $2}') 
+#hora_i=${temps_i:0:2}
+#minut_i=${temps_i:3:2}
 
 #amb stat -c  %y podem saber la modificació del fitxer
 #resolv.conf es el que miro per saber si s'ha canviat el fitxer
 
-if [ ! -d "/back" ]; then
 
-	mkdir "/back"
-fi
-mkdir "/back/$DATE"
 
 #Busquem als directoris
 
