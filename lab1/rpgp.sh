0c28ef26375b5f731ebc67a747828720a2946cea
abdevcode <abdevcode@gmail.com>
2020-03-25 02:11:52 +0100

    abdevcode: lab2-ex2 practicament acabat a falta de la instal.lacio per fer que sigui automatic

--- lab1/rpgp.sh
+++ lab1/rpgp.sh
@@ -10,6 +10,9 @@
 
 #Realitzeu   un   script   anomenat  rpgp.sh  (restaura   propietari,   grup   ipermisos) que rebi per paràmetre un fitxer amb la informació de sortidade l'exercici anterior i demani confirmació per a modificar els permisos,propietari i/o grup dels fitxers del sistema per tal que les dues cosescoincideixin. L'script ha de mostrar la informació actual i la informació del fitxer, en cas que sigui diferent, i demanarà confirmació per provocar quesigui igual. L'script informarà per pantalla si falta algun fitxer que estiguien el fitxer passat per paràmetre i ara no estigui al sistema, tambéinformarà un resum dels canvis que s'han realitzat.
 
+# rpgp.sh <fitchero entrada> [<opcion defecto>]
+# <fitchero entrada>: Fichero salida que genera el script gpgp.sh
+# <opcion defecto>: Opcion por defecto para modificar de manera automatica los ficheros, si no se pasa el argumento se preguntara antes de realizar cualquier accion
 
 # Funcion para pedir la confirmacion al usuario antes de realizar un cambio en el fichero
 ask_user_change_file() {
@@ -29,67 +32,88 @@ ask_user_change_file() {
 }
 
 
-#Verificacio de les opcions i dels parametres 
+# Verificacio de les opcions i dels parametres 
 if [ $# -eq 0 ]; then 
 	#Treiem error per canal stderr 
 	echo "Error: sin parametros." >&2
-else
-	fitxer=$1
-	
-	#Validar que existeix el fitxer
-	if [ -f $fitxer ]; then
-		#Separador de camp (IFS) - per assignar car especials amb $''
-		IFS=$'\n'
-		for line in $(cat $fitxer); do
-			num_espacios=$(echo $line | wc -w)
-			let ini=1
-			let end=$(echo "$num_espacios-3" | bc)
+	exit 1
+fi
 
-			ruta=$(echo $line | cut -d' ' -f $ini-$end)
+fitxer=$1
+opcion_param=$2
 
-			let ini=$end+1
-			let end=$end+1
-			prop_ant=$(echo $line | cut -d' ' -f $ini-$end)
+# Permitir establecer una opcion por defecto para el script
+if [ ! -z "$opcion_param" ]; then
+    case "$opcion_param" in
+		si | s ) 
+			mod_file=1;;
+		no | n) 
+			# Si no vamos a realizar ningun cambio, salimos
+			exit 0;;
+		* ) # Si la opcion no es valida, salimos!
+			echo "Opcio no valida! :/">&2
+			exit 1
+			;;
+	esac
+fi
 
-			let ini=$end+1
-			let end=$end+1
-			grup_ant=$(echo $line | cut -d' ' -f $ini-$end)
+#Validar que existeix el fitxer
+if [ -f $fitxer ]; then
+	#Separador de camp (IFS) - per assignar car especials amb $''
+	IFS=$'\n'
+	for line in $(cat $fitxer); do
+		num_espacios=$(echo $line | wc -w)
+		let ini=1
+		let end=$(echo "$num_espacios-3" | wc -w)
 
-			let ini=$end+1
-			let end=$end+1
-			perm_ant=$(echo $line | cut -d' ' -f $ini-$end)
+		ruta=$(echo $line | cut -d' ' -f $ini-$end)
 
-			if [ -e $ruta ]; then
-				prop_act=$(stat "$ruta" --printf=%U)
-				grup_act=$(stat "$ruta" --printf=%G)
-				perm_act=$(stat "$ruta" --printf=%a)
+		let ini=$end+1
+		let end=$end+1
+		prop_ant=$(echo $line | cut -d' ' -f $ini-$end)
 
-				if [ "$prop_act" != "$prop_ant" ] || [ "$grup_act" != "$grup_ant" ] || [ "$perm_act" != "$perm_ant" ]; then
-					echo "=================================="
-					echo "ruta: $ruta"
-					echo -e "Anterior: \t $prop_ant \t $grup_ant \t $perm_ant"
-					echo -e "Actual:   \t $prop_act \t $grup_act \t $perm_act"
+		let ini=$end+1
+		let end=$end+1
+		grup_ant=$(echo $line | cut -d' ' -f $ini-$end)
 
+		let ini=$end+1
+		let end=$end+1
+		perm_ant=$(echo $line | cut -d' ' -f $ini-$end)
+
+		if [ -e $ruta ]; then
+			prop_act=$(stat "$ruta" --printf=%U)
+			grup_act=$(stat "$ruta" --printf=%G)
+			perm_act=$(stat "$ruta" --printf=%a)
+
+			if [ "$prop_act" != "$prop_ant" ] || [ "$grup_act" != "$grup_ant" ] || [ "$perm_act" != "$perm_ant" ]; then
+				echo "=================================="
+				echo "ruta: $ruta"
+				echo -e "Anterior: \t $prop_ant \t $grup_ant \t $perm_ant"
+				echo -e "Actual:   \t $prop_act \t $grup_act \t $perm_act"
+
+				# En caso de no tener el segundo parametro, le preguntamos al usuario que quiere hacer
+				if [ -z "$opcion_param" ]; then
 					# Llamada a la funcion para preguntar al usuario
 					ask_user_change_file
-
-					if [ $? -eq 1 ]; then
-						echo "Modifiquem els fitxers"
-
-						# Modificamos el propietario, el grupo y los permisos
-						$(chown "$prop_ant" "$ruta") # Ponemos el propietario anterior
-						$(chgrp "$grup_ant" "$ruta") # Ponemos el grupo anterior
-						$(chmod "$perm_ant" "$ruta") # Ponemos los permisos anteriores
-					fi
+					mod_file=$?
 				fi
-			else
-				#echo "Error: No existe dir/fit de la path: $ruta" >&2 
-				echo "=================================="
-				echo "Error: No existe dir/fit de la path: $ruta
-"
+
+				if [ $mod_file -eq 1 ]; then
+					echo "Modifiquem els fitxers"
+
+					# Modificamos el propietario, el grupo y los permisos
+					$(chown "$prop_ant" "$ruta") # Ponemos el propietario anterior
+					$(chgrp "$grup_ant" "$ruta") # Ponemos el grupo anterior
+					$(chmod "$perm_ant" "$ruta") # Ponemos los permisos anteriores
+				fi
 			fi
-		done
-	else 
-		echo "Error: no existe el fichero especificado por parametro." >&2
-	fi
+		else
+			#echo "Error: No existe dir/fit de la path: $ruta" >&2 
+			echo "=================================="
+			echo "Error: No existe dir/fit de la path: $ruta"
+		fi
+	done
+else 
+	echo "Error: no existe el fichero especificado por parametro." >&2
 fi
+
--- lab2/ex1/copia_seg_sistema.sh
+++ lab2/ex1/copia_seg_sistema.sh
@@ -7,9 +7,6 @@
 #    2. sudo chmod +x /usr/bin/copia_seg_sistema.sh
 
 
-
-
-
 # Es vol programar un servei que s'executi al aturar el sistema, aquest script 
 # ha de crear un fitxer amb el path absolut dels fitxers modificats des de l’última 
 # vegada que es va engegar el sistema  que estiguin sota dels directoris /usr i del /home, 
@@ -21,19 +18,15 @@
 # que s'ha fet la còpia en el format: 'aammddhhmm'. Aquesta servei el volem fer seguint el 
 # format de serveis systemd.
 
-
-
 # Crear la carpeta de las copias de seguridad
-
 mkdir -p "/back" # -p : Crea els directoris pare necessaris, sense mostrar errors si aquests ja existeixen.
 date_act=$(date '+%y%m%d%H%M')
 dir_back_date="/back/$date_act"
 dir_back_temp="/back/temp"
-#mkdir $dir_back_date
 mkdir $dir_back_temp
 
-
-
+# Creamos el fichero donde vamos a guardar la copia de seguretat
+tar -T -rf "$dir_back_date.tar.gz"
 
 
 # Para saber la fecha actual
@@ -42,7 +35,7 @@ udate_start=$(date -d "$up_time" +%s) # Hora UNIX de la fecha
 
 # Directorios que queremos guardar separados por espacio en blanco
 #dir_save="/usr /home"
-dir_save="/home"
+dir_save="/test"
 
 for directori_busca in $dir_save; do
 	for ruta in $(find $directori_busca -type d -name ".*" -prune -o -type f \! -name ".*" -print); do
@@ -52,21 +45,16 @@ for directori_busca in $dir_save; do
 			
 			if [ $udate_file -gt $udate_start ]; then
 				echo $ruta >> "$dir_back_temp/paths.txt"
-				cp "$ruta" "$dir_back_temp"
-
-				# fer tar directament -T 
+				tar -rf "$dir_back_date.tar.gz" $ruta
 			fi 
 		fi
 	done
 done
 
-
-#Executem el script per guardar els permisos i tal i ho redirigim a un fitxer de text
+# Executem el script per guardar els permisos i tal i ho redirigim a un fitxer de text
 . gpgp.sh "$dir_back_temp/paths.txt" >> "$dir_back_temp/gpgp.out"
 
-#comprimim en tgz
-echo "tar -czvf "$dir_back_date.tar.gz" "$dir_back_temp""
-tar -czvf "$dir_back_date.tar.gz" "$dir_back_temp"
-
-
+# Comprimimos los ficheros generados 
+tar -C "$dir_back_temp/" -rf "$dir_back_date.tar.gz" "paths.txt" "gpgp.out"
+rm -r "$dir_back_temp/"
 
