3d3d2ab487417138f74e448aa06403dc0864f485
MaxLeat <57955198+MaxLeat@users.noreply.github.com>
2020-03-27 14:40:30 +0100

    Afegir que només siguin els del dia actual

--- lab3/Script3.sh
+++ lab3/Script3.sh
@@ -7,32 +7,45 @@
 
 #Data: 2/03/2020 
 #Versio: 1.2
-
-# Volem saber el nom de la comanda i el nom de l’usuari que han
-#generats els processos que han estat engegats entre les 13:00 i les
-#14:5
+#Que fa l'script bla bla bla
 
 echo "Els processos creats entre les 13.00 i les 15.00 son:"
 echo " COMANDA -- USUARI"
-
 h_inici=12
 h_fi=15
 
+data_actual=$(date)
+dia_actual=$(echo $data_actual | awk '{print $3}')
+mes_actual=$(echo $data_actual | awk '{print $2}')
+mes_actual=$(echo -n ${mes_actual:0:1} | tr '[:lower:]' '[:upper:]' ; echo ${mes_actual:1} | tr '[:lower:]' '[:lower:]')
 #Obtinc els processos que 
 IFS=$'\n'
 i=0
-for proces in $(ps aux | awk '{print $2, $9, $1, $11}'); do
+for proces in $(lastcomm); do
+	#Conto quantes paraules te la resposta
+	paraules=$(echo $proces | wc -w )
+	#Depenent de les paraules que tingui haure d'agafar de diferents posicions
+	inici=$(echo $proces | awk -v final="$paraules" '{print $final}')
+	let paraules=$paraules-1
+	dia=$(echo $proces | awk -v final="$paraules" '{print $final}')
+	let paraules=$paraules-1
+	mes=$(echo $proces | awk -v final="$paraules" '{print $final}')
+	let paraules=$paraules-5
+	usuari=$(echo $proces | awk -v pos_usu="$paraules" '{print $pos_usu}')	
 	#M'he de saltar el primer perque es la guia
+	
 	if [ $i -gt 0 ]; then
-		inici=$(echo $proces | awk '{print $2}')
-		hora=${inici:0:2}
-		minut=${inici:3:5}
-		if [ $hora -gt $h_inici ]; then 
-			if [ $hora -lt $h_fi ]; then 
-				#Si hem entrat aqui el proces s'ha creat al marge especificat
-				usuari=$(echo $proces | awk '{print $3}')
-				comanda=$(echo $proces | awk '{print $4}')
-				echo "$comanda -- $usuari"
+		if [ $dia -eq $dia_actual ]; then
+			if [ "$mes" = "$mes_actual" ]; then
+				hora=${inici:0:2}
+				minut=${inici:3:5}
+				if [ $hora -gt $h_inici ]; then 
+					if [ $hora -lt $h_fi ]; then 
+						#Si hem entrat aqui el proces s'ha creat al marge especificat
+						comanda=$(echo $proces | awk '{print $1}')
+						echo "$comanda -- $usuari"
+					fi
+				fi
 			fi
 		fi
 	fi
