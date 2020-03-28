376b46bfcac977402666f684499272591951da41
MaxLeat <57955198+MaxLeat@users.noreply.github.com>
2020-03-28 11:20:39 +0100

    Oriol: Petits canvis a bfit.sh
    Millor comprovació de parametres

--- lab1/bfit.sh
+++ lab1/bfit.sh
@@ -8,18 +8,19 @@
 #Data: 15/02/2020 
 #Versio: 1
 
-#Script que buidia un fitxer passat per paràmetre.
+#Que fa l'script bla bla bla
 
 #Verificacio de les opcions i dels parametres 
-if [ $# -eq 0 ]; then 
-	#Treiem error per canal stderr 
-	echo "Error: sin parametros." >&2
-else
+if [ $# -eq 1 ]; then 
 	fitxer=$1
 	#Validar que existeix el fitxer
 	if [ -f $fitxer ]; then
-		(cat /dev/null > $fitxer)
+		cat /dev/null > $fitxer
 	else 
-		echo "Error: no existe el fichero especificado por parametro." >&2
+		echo "Error: no existe el fichero especificado por parametro o es un directorio" >&2
 	fi
+else
+	
+	#Treiem error per canal stderr 
+	echo "Error: se accepta solo un parametro" >&2
 fi
