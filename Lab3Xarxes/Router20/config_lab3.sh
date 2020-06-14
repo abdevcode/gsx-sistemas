d682d0797bc78fcf28451a65baee868a340d6293
MaxLeat <57955198+MaxLeat@users.noreply.github.com>
2020-06-14 18:34:03 +0200

    Error del prepend arreglat

--- Lab3Xarxes/Router20/config_lab3.sh
+++ Lab3Xarxes/Router20/config_lab3.sh
@@ -3,6 +3,9 @@
 echo "Per realitzar aquesta part primer has de fer la configuració DNS del Bastió"
 echo "Realitza la configuració DNS (si no ho has fet) i prem ENTER..."
 read dummy
+
+sed -i 's/domain-name-servers, / /g' /etc/dhcp/dhclient.conf
+sed -i 's/#supersede domain-name "fugue.com home.vix.com";/supersede domain-name "inside36.gsx";/g' /etc/dhcp/dhclient.conf
 sed -i 's/#prepend domain-name-servers 127.0.0.1;/prepend domain-name-servers 203.0.113.9;/g' /etc/dhcp/dhclient.conf
 
 systemctl restart networking
