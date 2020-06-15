06520915a1c7f349c1f13237fc66fdebce775cfc
abdevcode <abdevcode@gmail.com>
2020-06-16 00:59:59 +0200

    Limitamos el icmp

--- Lab4Xarxes/Router/config_lab4.sh
+++ Lab4Xarxes/Router/config_lab4.sh
@@ -2,6 +2,13 @@
 
 . ./def_interficies.sh
 
+iptables -F
+iptables -X
+
+#Com que treballem amb snat reestablim la taula de nat
+iptables -t nat -F
+iptables -t nat -X
+
 iptables --flush
 iptables -t nat --flush
 
@@ -17,6 +24,7 @@ iptables -A OUTPUT -o lo -j ACCEPT
 #SNAT:
 ipe=$(ifconfig $IFISP | grep "inet " | awk '{print $2}')
 iptables -t nat -A POSTROUTING -o enp0s3 -j SNAT --to-source $ipe
+
 #Regles default:
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
@@ -33,6 +41,13 @@ iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 #Conexions entrants i sortitns ssh
 iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+
+# ------>
+# Make ICMP traffic of that type not trackable by state tracking.
+iptables -t raw -I PREROUTING -m icmp -p icmp --icmp-type any -j NOTRACK
+# <------
+
 
 #Sortir icmp echo-rquest
 iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
