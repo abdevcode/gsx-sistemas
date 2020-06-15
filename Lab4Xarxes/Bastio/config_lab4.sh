032621b2ab3b285217895e15394776d4f9c4119e
da <ad>
2020-06-15 19:03:43 +0200

    DNS quiery

--- Lab4Xarxes/Bastio/config_lab4.sh
+++ Lab4Xarxes/Bastio/config_lab4.sh
@@ -4,7 +4,7 @@ iptables --flush
 iptables -t nat --flush
 
 #Default
-iptables -P OUTPUT ACCEPT 
+iptables -P OUTPUT DROP 
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 
@@ -13,7 +13,10 @@ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #Queries DNS
+iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+iptables -A INPU -p udp --dport 53 -j ACCEPT
 #Sortides HTTP HTTPS
 iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
