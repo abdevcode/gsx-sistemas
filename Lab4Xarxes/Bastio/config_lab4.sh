f1fe388f049c8de3db3e5c66ad91c8e804706711
dasd <dasd>
2020-06-15 15:52:51 +0200

    Bastio Tallafocs

---
+++ Lab4Xarxes/Bastio/config_lab4.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+iptables --flush
+iptables -t nat --flush
+
+#Default
+iptables -P OUTPUT ACCEPT 
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+
+#Regles Default:
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+#Queries DNS
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+#Sortides HTTP HTTPS
+iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
+#Entrades i sortides SSH
+iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+
+
