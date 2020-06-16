b0adb0378cbd110d43c7f402d91c0acc83d42bee
RamonDonadeu <43962562+RamonDonadeu@users.noreply.github.com>
2020-06-16 18:26:59 +0200

    Update config_lab4.sh

--- Lab4Xarxes/Bastio/config_lab4.sh
+++ Lab4Xarxes/Bastio/config_lab4.sh
@@ -16,7 +16,7 @@ iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
 iptables -A INPUT -p tcp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
-iptables -A INPU -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
 #Sortides HTTP HTTPS
 iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
