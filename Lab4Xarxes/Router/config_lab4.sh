a868b7ec0b4a9ec3774fbde4c2c94b8c22518df7
MaxLeat <57955198+MaxLeat@users.noreply.github.com>
2020-06-16 10:54:39 +0200

    ping arreglat

--- Lab4Xarxes/Router/config_lab4.sh
+++ Lab4Xarxes/Router/config_lab4.sh
@@ -2,13 +2,6 @@
 
 . ./def_interficies.sh
 
-iptables -F
-iptables -X
-
-#Com que treballem amb snat reestablim la taula de nat
-iptables -t nat -F
-iptables -t nat -X
-
 iptables --flush
 iptables -t nat --flush
 
@@ -24,15 +17,15 @@ iptables -A OUTPUT -o lo -j ACCEPT
 #SNAT:
 ipe=$(ifconfig $IFISP | grep "inet " | awk '{print $2}')
 iptables -t nat -A POSTROUTING -o enp0s3 -j SNAT --to-source $ipe
-
 #Regles default:
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #Queries DNS
-iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 
 #Conexions sortitns http https
 iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
@@ -42,22 +35,17 @@ iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-
-# ------>
-# Make ICMP traffic of that type not trackable by state tracking.
-iptables -t raw -I PREROUTING -m icmp -p icmp --icmp-type any -j NOTRACK
-# <------
-
-
 #Sortir icmp echo-rquest
 iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type 0 -j ACCEPT
 
 #Limitar entrades echo-request a 5 per minut
-iptables -A INPUT -p icmp --icmp-type echo-request --match limit --limit 5/m --limit-burst 10 -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-request \
+    -m limit --limit 5/m --limit-burst 10 -j ACCEPT
+iptables -A INPUT -p icmp -j DROP
 
 #Forwards Bastio
 iptables -A FORWARD -p udp -i $IFINSIDE -o $IFISP --dport 53 -j ACCEPT
-iptables -A FORWARD -p tcp -i $IFINSIDE -o $IFISP --dport 53 -j ACCEPT
 iptables -A FORWARD -p tcp -i $IFINSIDE -o $IFISP --dport 80 -j ACCEPT
 iptables -A FORWARD -p tcp -i $IFINSIDE -o $IFISP --dport 443 -j ACCEPT
 iptables -A FORWARD -p tcp -i $IFINSIDE -o $IFISP --dport 22 -j ACCEPT
