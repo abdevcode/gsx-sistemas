3fe15e7bae5b10d3831638a25157818efb01c537
MaxLeat <57955198+MaxLeat@users.noreply.github.com>
2020-06-17 12:32:51 +0200

    My mistake guachos

--- Lab4Xarxes/Bastio/config_lab4.sh
+++ Lab4Xarxes/Bastio/config_lab4.sh
@@ -14,12 +14,16 @@ iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #Queries DNS
 iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
-iptables -A INPUT -p tcp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
 iptables -A INPUT -p udp --dport 53 -j ACCEPT
 #Sortides HTTP HTTPS
 iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 3128 -j ACCEPT
 #Entrades i sortides SSH
 iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+
+
