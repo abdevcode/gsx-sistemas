3c5c199edbce83b2824857e9b34d7483323b10ea
ramon <ramon>
2020-06-19 19:12:53 +0200

    restaura intern arreglo

--- gsx-xarxes/intern/restaura_inici.sh
+++ gsx-xarxes/intern/restaura_inici.sh
@@ -4,14 +4,10 @@ if [ -f "interfaces.confbak" ]; then
     cp interfaces.bak ~/etc/networks/interfaces
 fi
 
-if [ -f "./dhclienthostname.bak" ]; then
+if [ -f "dhclienthostname.bak" ]; then
     cp dhclienthostname.bak ~/etc/dhcp/dhclient-exit-hooks.d/hostname
 fi
 
-if [ -f "hosts.bak" ]; then
-    cp hostname.bak ~/etc/hostname
-fi
-
-if [ -f "hosts.bak" ]; then
+if [-f "hosts.bak" ]; then
     cp hosts.bak ~/etc/hosts
 fi
