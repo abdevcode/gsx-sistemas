5513f7cbfd04731fcaa1b683ecf14f16cde0b49d
RamonDonadeu <43962562+RamonDonadeu@users.noreply.github.com>
2020-06-19 18:53:05 +0200

    Update restaura_inici.sh

--- gsx-xarxes/router/restaura_inici.sh
+++ gsx-xarxes/router/restaura_inici.sh
@@ -1,3 +1,21 @@
 #!bin/sh
 
+if [ -f "interfaces.bak" ]; then
+    cp interfaces.bak /etc/network/interfaces
+fi
 
+if [ -f "resolv.confbak" ]; then
+    cp resolv.confbak /etc/resolv.conf
+fi
+
+if [ -f "dhclient.confbak" ]; then
+    cp dhclient.confbak /etc/dhcp/dhclient.conf
+fi
+
+if [ -f "hostname.bak" ]; then
+    cp hostname.bak /etc/hostname
+fi
+
+if [ -f "hosts.bak" ]; then
+    cp hosts.bak /etc/hosts
+fi
