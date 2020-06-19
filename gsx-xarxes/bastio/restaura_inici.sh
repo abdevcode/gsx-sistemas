722f85efa48037a88100b66aa054763f3c05257a
abdevcode <abdevcode@gmail.com>
2020-06-20 01:03:43 +0200

    Fer copies de backup

---
+++ gsx-xarxes/bastio/restaura_inici.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# lab 1
+cp /etc/network/interfaces.bak /etc/network/interfaces
+
+# lab 2
+cp /etc/dhcp/dhcpd.conf.bak /etc/dhcp/dhcpd.conf
+cp /etc/default/isc-dhcp-server.bak /etc/default/isc-dhcp-server
+
+# lab 3
+cp /etc/bind/named.conf.local.bak /etc/bind/named.conf.local
+cp /etc/bind/named.conf.options.bak /etc/bind/named.conf.options
+cp /etc/resolv.conf.bak /etc/resolv.conf
+cp /etc/hostname.bak /etc/hostname
+cp /etc/hosts.bak /etc/hosts
+
+# lab 4
+
+# lab 5
+cp /etc/squid/squid.conf.bak /etc/squid/squid.conf
