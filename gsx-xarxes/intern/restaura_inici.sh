bc1e75e33d7a9e6758fd9f3e7cdca64a7b3cb915
RamonDonadeu <43962562+RamonDonadeu@users.noreply.github.com>
2020-06-19 18:51:30 +0200

    Update restaura_inici.sh

---
+++ gsx-xarxes/intern/restaura_inici.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -f "interfaces.confbak" ]; then
+    cp interfaces.bak ~/etc/networks/interfaces
+fi
+
+if [ -f "./dhclienthostname.bak" ]; then
+    cp dhclienthostname.bak ~/etc/dhcp/dhclient-exit-hooks.d/hostname
+fi
+
+if [ -f "hosts.bak" ]; then
+    cp hostname.bak ~/etc/hostname
+fi
+
+if [ -f "hosts.bak" ]; then
+    cp hosts.bak ~/etc/hosts
+fi
